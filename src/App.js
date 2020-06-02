9aae2dcf64c739a14b1b7390aeea29c8005720c3
koashima <48111257+koashima@users.noreply.github.com>
2020-06-02 14:46:42 -0700

    functionality of CircleSelector done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import CircleSelector from './CircleSelector';
 import Circles from './Circles';
+
 class App extends Component {
+  
+  state = {
+    select: 1,
+  };
+
+  handleSelected = (circle) => {
+    this.setState((state) => ({select: circle}))
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
         <main>
-          <CircleSelector />
-          <Circles />
+          <CircleSelector select={this.state.select} handleSelected={this.handleSelected}/>
+          <Circles select={this.state.selCircle} />
         </main>
       </div>
     );
--- src/CircleSelector.js
+++ src/CircleSelector.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './CircleSelector.css'
+
+
 const CircleSelector = (props) => { 
 
-
   return (
-    <div>
-
+    <div className="CircleSelector">
+      {props.select === 1 ? (
+        <button className="selected">CIRCLE 1 SELECTED</button>
+      ) : (
+        <button onClick={ () => props.handleSelected(1)}>SELECT CIRCLE 1</button>
+      )}  
+    
+      {props.select === 2 ? (
+          <button className="selected">CIRCLE 2 SELECTED</button>
+        ) : (
+          <button onClick={() => props.handleSelected(2)}>SELECT CIRCLE 2</button>
+        )}  
+      
+      {props.select === 3 ? (
+          <button className="selected">CIRCLE 3 SELECTED</button>
+        ) : (
+          <button onClick={() => props.handleSelected(3)}>SELECT CIRCLE 3</button>
+        )}  
+      
+      {props.select === 4 ? (
+          <button className="selected">CIRCLE 4 SELECTED</button>
+        ) : (
+          <button onClick={() => props.handleSelected(4)}>SELECT CIRCLE 4</button>
+        )}  
     </div>
   )
 }
