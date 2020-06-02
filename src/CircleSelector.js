ca362d25b215da864b023cb14ee3a0444235fd1d
koashima <48111257+koashima@users.noreply.github.com>
2020-06-02 13:27:20 -0700

    Sets up Components

---
+++ src/CircleSelector.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CircleSelector.css'
+const CircleSelector = (props) => { 
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CircleSelector; 
---
+++ src/Circles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Circles = (props) => { 
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Circles; 
