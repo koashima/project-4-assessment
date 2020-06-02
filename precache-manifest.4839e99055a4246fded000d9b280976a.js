129aad8a39ad93d8f7453501919a51ea44fec2cf
koashima <48111257+koashima@users.noreply.github.com>
2020-06-02 15:00:46 -0700

    Updates

---
+++ precache-manifest.4839e99055a4246fded000d9b280976a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f43054b15a03aafc3975227059e0302",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "d11511fb0d838e51dcc2",
+    "url": "/project-4-assessment/static/css/main.f815e08f.chunk.css"
+  },
+  {
+    "revision": "3357667a71712448bd75",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d11511fb0d838e51dcc2",
+    "url": "/project-4-assessment/static/js/main.afe973a9.chunk.js"
+  },
+  {
+    "revision": "11176ca57580c991af7e",
+    "url": "/project-4-assessment/static/js/runtime-main.548f4f76.js"
+  }
+]);
